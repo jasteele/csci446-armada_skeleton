878e7ac1d06577787e9065651a45d35874c8e7f0
Jared Steele <jasteele@mines.edu>
2011-04-10 18:40:50 -0600

    Password reset email changed. Resolves #1.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   
   def password_reset_instructions(user)
     subject       "Password Reset Instructions"
-    from          "FIXME <no-reply@FIXME.com>"
+    from          "No Reply <no-reply@pure-ice-359.com>"
     recipients    user.email
     sent_on       Time.now
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
