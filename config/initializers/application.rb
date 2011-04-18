181aefcf123ed4a79c85b1fdba1327eac73e271e
Jared Steele <jasteele@mines.edu>
2011-04-17 19:24:16 -0600

    added projectile counts and date time formatting to index partial

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,3 +1,4 @@
 APP_TITLE = "Armada Projectiles"
 Time::DATE_FORMATS[:datetime] = "%B %d, %Y %I:%M%p"
 Time::DATE_FORMATS[:short_datetime] = "%b %d, %Y %I:%M%p"
+Time::DATE_FORMATS[:normal] = "%B %d, %Y"
