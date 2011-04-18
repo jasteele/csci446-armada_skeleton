9e53c956efe7a1eae41490b4de55c0097186b6dd
Jared Steele <jasteele@mines.edu>
2011-04-17 18:53:01 -0600

    shortened a seed description because heroku is strict on the 255 limit of characters.

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,7 @@ photo = File.open(File.join(Rails.root, 'public', 'images',
 'pirate_canon.jpg')) 
 # Seed the projectiles
 one = Projectile.new(:name => 'Throwing Knife', :ptype => 'Knife', :cost => 13, :description => 'Throwing knives are knives that are specially designed and weighted so that they can be thrown effectively.', :user => u[0])
-two = Projectile.new(:name => 'Poison Arrow', :ptype => 'Arrow', :cost => 1, :description => 'Poison arrows have been used by hunter-gatherer peoples worldwide and are still in use in areas of South America, Africa and Asia. The most awesome poisons are secreted from the skin of the poison dart frog and curare, a general term for a range of plant-derived arrow poisons used by the indigenous peoples of South America.', :user => u[0])
+two = Projectile.new(:name => 'Poison Arrow', :ptype => 'Arrow', :cost => 1, :description => 'Poison arrows have been used by hunter-gatherer peoples worldwide and are still in use in areas of South America, Africa and Asia. The most awesome poisons are secreted from the skin of the poison dart frog and curare.', :user => u[0])
 three = Projectile.new(:name => 'Flaming Arrow', :ptype => 'Arrow', :cost => 1, :description => "Simple: 1) Light an arrow on fire. 2) Shoot onto enemy's roof. 3) ????? 4) Profit! ", :user => u[1])
 four = Projectile.new(:name => 'Blowgun', :ptype => 'Dart', :cost => 2, :description => "A blowgun is a simple weapon consisting of a small tube for firing light projectiles, or darts.", :user => u[2])
 one.photo_file_name = photo
